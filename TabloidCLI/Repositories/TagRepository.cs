fe24d4e3379458cffbee392de0c8155185f96561
Alex Bishop <alexb98@gmail.com>
2023-06-15 10:06:17 -0500

    Search post functionality added

--- TabloidCLI/Repositories/TagRepository.cs
+++ TabloidCLI/Repositories/TagRepository.cs
@@ -150,5 +150,42 @@ namespace TabloidCLI
                 }
             }
         }
+
+        public SearchResults<Post> SearchPosts(string tagName)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT p.Id, p.Title, p.Url, p.PublishDateTime, p.AuthorId, p.BlogId FROM Post p
+                                        LEFT JOIN PostTag pt on p.Id = pt.PostId
+                                        LEFT JOIN Tag t on t.Id = pt.TagId
+                                        WHERE t.Name LIKE @name";
+                    cmd.Parameters.AddWithValue("@name", $"%{tagName}%");
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    SearchResults<Post> results = new SearchResults<Post>();
+                    while (reader.Read())
+                    {
+                        Post post = new Post()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Url = reader.GetString(reader.GetOrdinal("Url")),
+                            PublishDateTime = reader.GetDateTime(reader.GetOrdinal("PublishDateTime")),
+                        };
+                        results.Add(post);
+                    }
+
+                    reader.Close();
+
+                    return results;
+                }
+            }
+        }
+
+
+
     }
 }
