3e3df04b7d501e52cdfab5e1650b578f44b5887a
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-06-15 10:44:58 -0400

    add tag to post

---
+++ TabloidCLI/Models/PostTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabloidCLI.Models
+{
+    public class PostTag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int TagId { get; set; }
+
+    }
+}
